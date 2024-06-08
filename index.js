baa1a401455be7c4dfa474d96fea212701ae441e
Ison024 <beniteziverson38@gmail.com>
2024-06-08 14:28:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const btnLikes1 = document.getElementById("btnLike1")
 const countLikes1 = document.getElementById("countLikes1")
 
-const submit = document.getElementById("submit")
-const comment = document.getElementById("comment")
-const commentbox = document.getElementById("commentbox")
-function submitComment(){
-  commentbox.textContent += comment.value.toString() + "\n"
-}
-submit.addEventListener("click", submitComment)
-
 function clickLike1(){
   let totalLikes1 = parseInt(countLikes1.value) + 1
   countLikes1.textContent = totalLikes1.toString()
@@ -16,6 +8,14 @@ function clickLike1(){
 
 const btnLikes2 = document.getElementById("btnLike2")
 const countLikes2 = document.getElementById("countLikes2")
+
+const submit = document.getElementById("submit")
+const comment = document.getElementById("comment")
+const commentbox = document.getElementById("commentbox")
+function submitComment(){
+  commentbox.textContent += comment.value.toString() + "\n"
+}
+submit.addEventListener("click", submitComment)
 
 function clickLike2(){
   let totalLikes2 = parseInt(countLikes2.value) + 1
